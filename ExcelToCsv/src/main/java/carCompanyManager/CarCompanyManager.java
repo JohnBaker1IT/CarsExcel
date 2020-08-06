d089734b3300de719f5afb99af5abe0b762ea026
andybakerman <67850347+andybakerman@users.noreply.github.com>
2020-08-06 12:54:03 -0500

    Made company creator

--- ExcelToCsv/src/main/java/carCompanyManager/CarCompanyManager.java
+++ ExcelToCsv/src/main/java/carCompanyManager/CarCompanyManager.java
@@ -11,35 +11,25 @@ import carCompanyManagerModel.CarCompaniesInfoPrint;
 import java.text.NumberFormat;
 
 public class CarCompanyManager {
-
-	public static void main(String[] args) {
-
+	
+	private static final CarCompany carCompany1 = null;
+	
+	static List<CarCompany> carCompanies = new LinkedList<>();  //PPT3 Array List to store all Bank Account Objects
+	
+	public static CarCompany createCarCompany(String name) {
 		
-        
-       
-		
-		List<CarCompany> carCompanies = new LinkedList<>();  //PPT3 Array List to store all Bank Account Objects
-		
-		CarCompany carCompany1 = new CarCompany();
-		carCompanies.add(carCompany1);
-		carCompany1.setCompanyName("Mercedes-Benz");
+		CarCompany carCompany = new CarCompany();
+		carCompanies.add(carCompany);
+		carCompany.setCompanyName(name);
 		List<String> addresses = List.of("124 Mulberry St.", "109 Cherry St.");
-		carCompany1.setAddresses(addresses);
-		carCompany1.mapAdd(carCompany1.getCompanyName(), carCompany1.getAddresses());
+		carCompany.setAddresses(addresses);
+		carCompany.mapAdd(carCompany.getCompanyName(), carCompany.getAddresses());
 		
-		CarCompany carCompany2 = new CarCompany();
-		carCompanies.add(carCompany2);
-		carCompany2.setCompanyName("BMW");
-		List<String> addresses2 = List.of("125 Mulberry St.", "110 Cherry St.");
-		carCompany2.setAddresses(addresses2);
-		carCompany2.mapAdd(carCompany2.getCompanyName(), carCompany2.getAddresses());
+		return carCompany;
 		
-		CarCompany carCompany3 = new CarCompany();
-		carCompanies.add(carCompany3);
-		carCompany3.setCompanyName("Acura");
-		List<String> addresses3 = List.of("126 Mulberry St.", "111 Cherry St.");//
-		carCompany3.setAddresses(addresses3);
-		carCompany3.mapAdd(carCompany3.getCompanyName(), carCompany3.getAddresses());
+	}
+	
+	public static void printCompanyInfo() {
 		
 		 System.out.println(carCompany1);
 	        
@@ -59,6 +49,31 @@ public class CarCompanyManager {
 		System.out.println("Added with interface: " + defaultFormat.format(intf)); 
 		
 		
+		
+	}
+
+	public static void main(String[] args) {
+
+		CarCompany carCompany1 = createCarCompany("Mercedes-Benz");
+		
+		
+		
+		CarCompany carCompany2 = new CarCompany();
+		carCompanies.add(carCompany2);
+		carCompany2.setCompanyName("BMW");
+		List<String> addresses2 = List.of("125 Mulberry St.", "110 Cherry St.");
+		carCompany2.setAddresses(addresses2);
+		carCompany2.mapAdd(carCompany2.getCompanyName(), carCompany2.getAddresses());
+		
+		CarCompany carCompany3 = new CarCompany();
+		carCompanies.add(carCompany3);
+		carCompany3.setCompanyName("Acura");
+		List<String> addresses3 = List.of("126 Mulberry St.", "111 Cherry St.");//
+		carCompany3.setAddresses(addresses3);
+		carCompany3.mapAdd(carCompany3.getCompanyName(), carCompany3.getAddresses());
+		
+		
+		printCompanyInfo();
 
 		
 		
--- ExcelToCsv/src/main/java/carCompanyManager/Controller.java
+++ ExcelToCsv/src/main/java/carCompanyManager/Controller.java
@@ -4,17 +4,31 @@ import java.util.Iterator;
 
 import com.to.excelToCsv.ExcelToCsv;
 
+import carCompanyManagerModel.CarCompany;
+
 public class Controller {
 
 	public static void main(String[] args) {
 		System.err.close();
 	    System.setErr(System.out);
 		
-		ExcelToCsv.readFile();
+		//Read the excel file to an linked list
+	    ExcelToCsv.readFile();
 		
+	     
 		for (Iterator i = ExcelToCsv.fileInfo.iterator(); i.hasNext();) {
-	         System.out.println(i.next());
-	      }
+	        //Get the company name 
+			System.out.println(i.next());
+	        
+			//Build the company object
+			CarCompany carCompany = CarCompanyManager.createCarCompany("Test");
+			CarCompanyManager.printCompanyInfo();
+			
+			break;
+			
+			
+			
+		}
         
         
 	}
